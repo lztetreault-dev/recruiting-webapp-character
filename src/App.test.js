555dda2ac6607287513d9fa22c09e874ce0c9478
Landon Tetreault <lztetreault@proton.me>
2024-11-09 21:41:47 -0700

    fix App test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Character Builder/i);
   expect(linkElement).toBeInTheDocument();
 });
